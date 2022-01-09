994cc6cc5002cebaecfdaa1ea6a1b652b1d57329
asratanzilla <tanzillanurdhiaasra51@gmail.com>
2022-01-09 18:00:20 +0700

    Initial commit

--- app/src/main/java/com/mws/responsimws/ApiEndPoint.kt
+++ app/src/main/java/com/mws/responsimws/ApiEndPoint.kt
@@ -4,7 +4,7 @@ class ApiEndPoint {
 
     companion object {
 
-        private val SERVER = "https://192.168.56.1/responsi/"
+        private val SERVER = "http://192.168.56.1/responsi/"
         val CREATE = SERVER+"create.php"
         val READ = SERVER+"read.php"
         val DELETE = SERVER+"delete.php"
